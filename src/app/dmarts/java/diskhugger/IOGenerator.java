6dd8ac0c5f9ba495efcdf50533ce9ee83cc5cda9
Farhan Sabbir Siddique <fsabbir@gmail.com>
2020-11-08 00:54:27 +0600

    pulled in the two methods first
    renamed the last_pos to LAST_POSITION to reflect object reference in new implementation

--- src/app/dmarts/java/diskhugger/IOGenerator.java
+++ src/app/dmarts/java/diskhugger/IOGenerator.java
@@ -3,34 +3,34 @@ package app.dmarts.java.diskhugger;
 import com.google.gson.JsonObject;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.channels.FileChannel;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-public  class IOGenerator implements Runnable{
+public  class IOGenerator implements Runnable {
 
         private File FILE;
-        private static long LAST_POSITION = 0;
-        public IOGenerator(String filename){
-            this.FILE = new File(filename);
+        private long LAST_POSITION = 0, MAX, MIN, BUFFER;
+
+        public IOGenerator(String filename) {
+                this.FILE = new File(filename);
+                if (Main.FILE_SIZE < Main.BUFFER_SIZE) {
+                        this.MAX = Main.FILE_SIZE;
+                        this.MIN = Main.FILE_SIZE;
+                        this.BUFFER = Main.FILE_SIZE;
+                } else {
+                        this.MIN = Main.BUFFER_SIZE / 3;
+                        this.MAX = Main.BUFFER_SIZE;
+                        this.BUFFER = Main.BUFFER_SIZE;
+                }
         }
+
         @Override
         public void run() {
 
         }
-}
-
 
 
 /*
-
-
 
 public class Main {
     private static long last_pos = 0;
@@ -127,3 +127,32 @@ public class Main {
 
 
  */
+
+        private Map<Long, Long> getRandNextWrite(long filesize, long buffer, int writecount) {
+                Map<Long, Long> ret = new HashMap<>(writecount);
+                long position;
+                while (writecount-- != 0) {
+                        position = this.LAST_POSITION;
+                        if ((buffer + position) >= filesize) {
+                                buffer = filesize - position;
+                        }
+                        ret.put(position, buffer);
+                        this.LAST_POSITION += buffer;
+                }
+                return ret;
+        }
+
+        private Map<Long, Long> getSeqNextWrite(long filesize, long buffer){
+                Map<Long, Long> ret = new HashMap<>();
+                long position = this.LAST_POSITION;
+                if (position == filesize){
+                        return ret;
+                }
+                if(position+buffer > filesize){
+                        buffer = filesize-position;
+                }
+                ret.put(position, buffer);
+                this.LAST_POSITION = position + buffer;
+                return ret;
+        }
+}
