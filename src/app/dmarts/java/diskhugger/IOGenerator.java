c2dff8176142cfad5190d17582811eb622f1c4c9
Farhan Sabbir Siddique <fsabbir@gmail.com>
2020-11-08 00:42:16 +0600

    Put a working code in comments. Need to make the change in main code now

--- src/app/dmarts/java/diskhugger/IOGenerator.java
+++ src/app/dmarts/java/diskhugger/IOGenerator.java
@@ -49,7 +49,7 @@ public  class IOGenerator implements Runnable{
                     channel.write(ByteBuffer.wrap(writeme.getBytes()));
                     long end = System.currentTimeMillis();
                     timetaken += end - start;
-                    if ((int)(end-start)>=max){
+                    if ((int)(end-start)>max){
                         max = (int)(end-start);
                         maxtime = start;
                         this.MAX_OBJ.addProperty("ms",max);
@@ -103,3 +103,105 @@ public  class IOGenerator implements Runnable{
             return WRITE_MAP;
         }
 }
+
+
+
+/*
+
+
+
+public class Main {
+    private static long last_pos = 0;
+    private static long max, min;
+    public static void main(String[] args) throws InterruptedException, IOException {
+        long filesize = 5368700L; //5368709120L
+        long buffer   = 8192;
+        boolean random = true;
+
+
+
+        if(filesize<buffer){
+            min = filesize;
+            max = filesize;
+            buffer = filesize;
+        }
+        else {
+            min = buffer/3;
+            max = buffer;
+        }
+
+        if(random) {
+            // buffer size is decided here;
+            if(filesize>buffer) {
+                buffer = (long) (Math.random() * (max - min + 1) + min);
+            }
+            System.out.println("Min: " + min + ", max: " + max + ", buffer: " + buffer + ", filesize: " + filesize);
+            Map<Long, Long> writemap = null;
+            FileChannel channel = new FileOutputStream(new File("writerme")).getChannel();
+            while (last_pos!=filesize){
+                writemap = (getRandNextWrite(filesize, buffer, 10));
+                for(long position:writemap.keySet()){
+                    System.out.println("Position: " + position + ", buffer: " + writemap.get(position));
+                    long writebuffer = writemap.get(position);
+                    channel.position(position);
+                    String data = LongStream.range(0,writebuffer).mapToObj(l -> "data").collect(Collectors.joining()).substring(0,(int)writebuffer);
+                    channel.write(ByteBuffer.wrap(data.getBytes()));
+                }
+                buffer = (long) (Math.random() * (max - min + 1) + min);
+            }
+            channel.close();
+            System.out.println("Last pos: " + last_pos + ", Remaining: " + (filesize-last_pos));
+            buffer = (filesize-last_pos);
+            if(buffer>0){
+                System.out.println();
+            }
+
+        }
+        else{
+            System.out.println("Min: " + min + ", max: " + max + ", buffer: " + buffer + ", filesize: " + filesize);
+            Map<Long, Long> writemap = null;
+            if(filesize>=buffer) {
+                for (int i = 0; i <= (filesize / buffer); i++) {
+                    writemap = getSeqNextWrite(filesize, buffer);
+                    System.out.printf("" + writemap);
+                }
+            }
+            else  {
+                for (int i = 0; i < (filesize / buffer); i++) {
+                    writemap = (getSeqNextWrite(filesize, buffer));
+                    System.out.printf("" + writemap);
+                }
+            }
+        }
+    }
+
+    private static Map<Long, Long> getRandNextWrite(long filesize, long buffer, int writecount){
+        Map<Long, Long> ret = new HashMap<>(writecount);
+        long position = 0;
+        while (writecount--!=0){
+            position = last_pos;
+            if ((buffer + position) >=filesize ){
+                buffer = filesize-position;
+            }
+            ret.put(position,buffer);
+            last_pos += buffer;
+        }
+        return ret;
+    }
+
+    private static Map<Long, Long> getSeqNextWrite(long filesize, long buffer){
+        Map<Long, Long> ret = new HashMap<>();
+        long position = last_pos;
+        if (position == filesize){
+            return ret;
+        }
+        if(position+buffer > filesize){
+            buffer = filesize-position;
+        }
+        ret.put(position, buffer);
+        last_pos = position + buffer;
+        return ret;
+    }
+
+
+ */
